f8995050fa4a4eabda3e4085e8e8fa533dc0e754
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-12 16:34:18 +0530

    multi operate

--- basics/higherorder.clj
+++ basics/higherorder.clj
@@ -3,3 +3,9 @@
 (println (operate clojure.string/upper-case "test string"))
 (println (operate clojure.string/reverse "test string"))
 (println (operate inc 30))
+
+(defn multi-operate [f & arg] (f arg))
+
+(println (multi-operate clojure.string/join "test string" " " "wow"))
+;; (println (multi-operate clojure.string/reverse "test string"))
+;; (println (multi-operate inc 30))
