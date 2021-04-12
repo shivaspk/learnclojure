ccee83a009250c88e8578bdcb2b97419b11132dc
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-12 15:32:31 +0530

    updated

--- basics/string.clj
+++ basics/string.clj
@@ -4,6 +4,14 @@
 (println (clojure.string/capitalize message))
 (println (clojure.string/replace message #"\w" (fn [ch] (do (println ch) "$"))))
 
-(defn touppercase [s]
+(defn to-upper-case [s]
     (clojure.string/upper-case s))
 ( println(touppercase "Hey we are trying new things!"))
+
+(defn get-words [s]
+    (count (clojure.string/split s #" ")))
+( println(getwords "Hey we are trying new things!"))
+
+(defn get-chars [s]
+    (count s))
+( println(getchars "Hey we are trying new things!"))
