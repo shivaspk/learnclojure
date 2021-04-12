05ff721e204f202be17a62b5ea699b69f512ca70
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-12 15:19:03 +0530

    updated

--- basics/string.clj
+++ basics/string.clj
@@ -3,3 +3,7 @@
 (println (clojure.string/upper-case message))
 (println (clojure.string/capitalize message))
 (println (clojure.string/replace message #"\w" (fn [ch] (do (println ch) "$"))))
+
+(defn touppercase [s]
+    (clojure.string/upper-case s))
+( println(touppercase "Hey we are trying new things!"))
