d28eca6d945b4121d15898293e347de0d2c06e89
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-14 09:46:16 +0530

    map and filter updated!

--- basics/mapandfilter.clj
+++ basics/mapandfilter.clj
@@ -3,3 +3,6 @@
 (println (map (fn [i] (* i 100)) [1 10 20 3 4 5]))
 
 (println (map count ["Test" "Now" "this" "rocks!"]))
+
+;; This is operates on multi sequence but note the result drops the non mapped values
+(println (map +  [1 10 20 3 4 5] [21 10 20 32]))
