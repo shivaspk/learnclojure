0c96a8e0a29570a97bb338f9938621f008c27578
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-14 12:24:41 +0530

    while loop with recur

--- basics/recursion.clj
+++ basics/recursion.clj
@@ -1,7 +1,13 @@
 (defn recradd [pending numbers]
 (if (first numbers)
-    (recradd (+ pending (first numbers))
+    (recur (+ pending (first numbers))
     (next numbers))
     pending))
 
 ( println ( recradd 0 (range 10000)))
+
+(loop [i 0]  
+    (when (< i 5)    
+      (println i)    
+      (recur (inc i)); loop i will take this value
+  ))
