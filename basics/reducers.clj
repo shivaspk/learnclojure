5160fa828a309eae33c047e4861b428b66a03ccf
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-14 11:16:48 +0530

    reducers updated!

--- basics/reducers.clj
+++ basics/reducers.clj
@@ -1,10 +1,14 @@
-(reduce + [1 2 3 4 5])
+(println (reduce + [1 2 3 4 5]))
 
-(reduce (fn [{:keys [minimum maximum]} n]
+(println (reduce (fn [{:keys [minimum maximum]} n]
             {
                 :minimum (if ( and minimum (> n minimum)) minimum n)
                 :maximum (if ( and maximum (< n maximum)) maximum n)
             }
         )
     {}
-    [100 2 3 4 500])
+    [100 2 3 4 500]))
+
+(println    (reduce #(assoc %1 %2 (inc (%1 %2 0)))
+    {}
+    (re-seq #"\w+" "THIS IS IS IS IS IS A NEW FILE UPDATED IS NOW NEW THINGS")))
