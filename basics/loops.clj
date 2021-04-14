6089b34c68f948837d626902150c4df871eaf08a
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-14 11:54:28 +0530

    loops updated!

---
+++ basics/loops.clj
@@ -0,0 +1,8 @@
+(doseq [n (range 10)] 
+    (println (str "line " n)))
+
+(take 3 (repeat "Hello"))
+
+(take 3 (repeatedly #(clojure.string/upper-case "hello")))
+
+(dotimes [n 5] (println "n is" n))
