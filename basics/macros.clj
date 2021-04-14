f7ac900630bacdf9117fbcb852616cf4930ddc7a
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-14 14:38:12 +0530

    use LET binding to fix the issue

--- basics/macros.clj
+++ basics/macros.clj
@@ -10,3 +10,15 @@
 (repeatme 5)
 
 (macroexpand '(repeatme 5))
+
+;; It's expected to fail as the S will be unresolved
+;;(defmacro repeatmystr [n s]
+;;    (cons 'do (repeat n '(println s))))
+;;(repeatmystr 5)
+
+(defmacro repeatmystr [n s]
+    (concat (list 'let ['str s])
+        (repeat n '(println str))))
+(repeatmystr 5 "TEST")
+
+(macroexpand '(repeatmystr 5 "TEST"))
