ad33b50b103ade882864ca9959b183bc96a7e992
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 14:47:03 +0530

    commit app

--- basics/fileops.clj
+++ basics/fileops.clj
@@ -2,13 +2,17 @@
 
 
 
-(spit "hello.txt" "This is a file from clojure!")
+;;(spit "hello.txt" "This is a file from clojure! THis is secondline")
 
 ;; read a file safely else print a error
 (try
 
-  (slurp "hello.txt")
+;;   (slurp "hello1.txt")
 
+(with-open [rdr (clojure.java.io/reader "hello.txt")]
+    (reduce conj [] (line-seq rdr)))
+
+  (catch java.io.FileNotFoundException e (println (str "File related Exception Occured: " (.getMessage e))))
   (catch Exception e (println (str "Exception Occured: " (.getMessage e))))
 
   (finally (println "Proceed...")))
