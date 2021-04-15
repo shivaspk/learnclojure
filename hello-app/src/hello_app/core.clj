5b8e894efd057b6ce8415c786213067fee1cfedf
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 15:03:15 +0530

    :require tested!

--- hello-app/src/hello_app/core.clj
+++ hello-app/src/hello_app/core.clj
@@ -1,7 +1,10 @@
 (ns hello-app.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!" args))
+  (println "Hello, World!")
+  (println (str/upper-case "now this works!"))
+  )
