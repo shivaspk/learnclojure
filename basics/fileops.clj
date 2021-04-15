aeb7566ad2153a97d51be23fe113706fa81f1471
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 14:32:33 +0530

    try catch

--- basics/fileops.clj
+++ basics/fileops.clj
@@ -1,2 +1,14 @@
 ;; creates a file called hello.txt and writes data
+
+
+
 (spit "hello.txt" "This is a file from clojure!")
+
+(try
+
+  (slurp "hello.txt")
+
+  (catch Exception e (println (str "Exception Occured: " (.getMessage e))))
+
+  (finally (println "Proceed...")))
+
