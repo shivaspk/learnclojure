89320b24c4db4c61aa928acdb09618cef18d6a80
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 12:44:16 +0530

    java documented

--- basics/javainterop.clj
+++ basics/javainterop.clj
@@ -1,3 +1,4 @@
+;; . notation for java classes
 (println (.toUpperCase "now this is java!"))
 
 (println (.toLowerCase "TESTING!"))
@@ -6,4 +7,14 @@
 
 (println ( type mystr))
 
+;;access system props
 (println(System/getProperty "java.vm.version"))
+
+;;access PI in two ways
+(println (Math/PI))
+(println (. Math PI))
+
+;;import java class
+(import java.util.Date)
+(let [date (Date.)]
+(println date))
