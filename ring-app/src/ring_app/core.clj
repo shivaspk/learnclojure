08c9877cb1dd0a7eebe03d7c3beaf8b3635c957b
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 17:49:53 +0530

    a form submit example

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,14 +1,31 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty])
+  (:require [ring.adapter.jetty :as jetty] 
+            [ring.middleware.params :as ringparams]
+            [ring.util.response :as ringresponse]
+            )
   (:gen-class))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<h1>Hello Ring!</h1> to: "  (:remote-addr request))})
+;; (defn handler [request]
+;;   {:status 200
+;;    :headers {"Content-Type" "text/html"}
+;;    :body (str "<h1>Hello Ring!</h1> to: "  (:query-string request))})
+
+(defn page [name]
+  (str "<html><body>"
+       (if name
+         (str "Nice to meet you, " name "!")
+         (str "<form>"
+              "Name: <input name='name' type='text'>"
+              "<input type='submit'>"
+              "</form>"))
+       "</body></html>"))
+
+(defn handler [{{name "name"} :params}]
+  (-> (ringresponse/response (page name))
+      (ringresponse/content-type "text/html")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Ring Starting....")
-  (jetty/run-jetty handler {:port  3000}))
+  (jetty/run-jetty  (-> handler ringparams/wrap-params) {:port  3000}))
