83bf15ade09042497bbdf188ad633196246ac031
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 11:26:23 +0530

    updated!

--- basics/watchers.clj
+++ basics/watchers.clj
@@ -7,3 +7,21 @@
 
 (reset! counter 100)
 (swap!  counter inc)
+
+(def myagent (agent 0))
+(add-watch myagent :watcher 
+    (fn [key agent oldval newval]
+        (println "Agent Changed!")
+        (println "old:" oldval)
+        (println "new:" newval)))
+
+(send-off myagent + 50)
+(println "immediate update")
+(println "immediate update")
+(println "immediate update")
+(send myagent + 500)
+(remove-watch myagent :watcher)
+(println "watcher removed")
+(println "async update")
+(println "async update" @myagent)
+;;(Thread/sleep 100) 
