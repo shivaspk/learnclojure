d3b3bc034a9c4f2bda4733dcdcf7d21e6a3e335a
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 17:33:23 +0530

    with client IP

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -5,7 +5,7 @@
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>Hello Ring!</h1>"})
+   :body (str "<h1>Hello Ring!</h1> to: "  (:remote-addr request))})
 
 (defn -main
   "I don't do a whole lot ... yet."
