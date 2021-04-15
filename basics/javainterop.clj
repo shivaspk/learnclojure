8d7baf6099f8096122266391e5c0b5bae3124637
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 12:38:30 +0530

    java interop

---
+++ basics/javainterop.clj
@@ -0,0 +1,9 @@
+(println (.toUpperCase "now this is java!"))
+
+(println (.toLowerCase "TESTING!"))
+
+(def mystr (new String "Test new!"))
+
+(println ( type mystr))
+
+(println(System/getProperty "java.vm.version"))
--- basics/metadata.clj
+++ basics/metadata.clj
@@ -1,5 +1,4 @@
 ;;create a collection with META
-
 (def myoddmap (with-meta [1 3 5] {:type "Odd Values" :datatype "Numbers"}))
 ;;print map
 (println myoddmap)
