abc630628355743a0998f2d7caa14babedf01b80
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 15:16:24 +0530

    added local time java 8

--- hello-app/src/hello_app/core.clj
+++ hello-app/src/hello_app/core.clj
@@ -1,10 +1,10 @@
 (ns hello-app.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str] [java-time :as time])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (str/upper-case "now this works!"))
-  )
+  (println (time/year))
+  (println (str/upper-case "now this works!")))
