f019869180cb7f378a713c0c29257f38964e5344
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-15 12:20:24 +0530

    comments added

--- basics/structs.clj
+++ basics/structs.clj
@@ -1,3 +1,4 @@
+;; define a struct
 (defstruct Employee :empid :empname :empage)
 
 (def emp1 (struct  Employee 1 "Siva" 35 ))
@@ -5,18 +6,22 @@
 (def emp3 (struct  Employee 3 "Raj" 30 ))
 (println emp1 emp2 emp3)
 
+;;creating a struct map
 (def emp (struct-map Employee :empid 100  :empage 32 :empname "Raju"))
 (println emp)
 
+;;accessing individual fields of struct map
 (println "ID: " (:empid emp))
 (println "Name: " (:empname emp))
 (println "Age: " (:empage emp))
 
+;; to show immutability 
 (assoc emp :empname "Rocky!")
 (println "ID: " (:empid emp))
 (println "Name: " (:empname emp))
 (println "Age: " (:empage emp))
 
+;; can only be done during new def! 
 (def newemp (assoc emp :empname "Rocky!"))
 (println "ID: " (:empid newemp))
 (println "Name: " (:empname newemp))
