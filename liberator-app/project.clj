4b11f70d8edd0b85f4538c7de88d1fdac7ef27f7
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-16 11:44:13 +0530

    liberator app

---
+++ liberator-app/project.clj
@@ -0,0 +1,12 @@
+(defproject liberator-app "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler liberator-app.core/handler}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [liberator "0.15.1"]
+                 [compojure "1.6.2"]
+                 [ring/ring-core "1.9.2"]]
+  :repl-options {:init-ns liberator-app.core})
---
+++ liberator-app/src/liberator_app/core.clj
@@ -0,0 +1,9 @@
+(ns liberator-app.core (:require [liberator.core :refer [resource defresource]]
+                                 [ring.middleware.params :refer [wrap-params]]
+                                 [compojure.core :refer [defroutes ANY]]))
+(defroutes app
+  (ANY "/" [] (resource)))
+
+(def handler
+  (-> app
+      wrap-params))
