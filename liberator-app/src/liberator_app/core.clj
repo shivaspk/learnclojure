ad4ed40d03563dfc5e95c1e0c92b5d146b70753f
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-16 12:11:12 +0530

    with param

--- liberator-app/src/liberator_app/core.clj
+++ liberator-app/src/liberator_app/core.clj
@@ -1,10 +1,19 @@
 (ns liberator-app.core (:require [liberator.core :refer [resource defresource]]
                                  [ring.middleware.params :refer [wrap-params]]
                                  [compojure.core :refer [defroutes ANY]]))
+
+(defresource parameter [txt]
+  :available-media-types ["text/plain"]
+  :handle-ok (fn [_] (format "The text is %s" txt)))
+
 (defroutes app
   (ANY "/" [] (resource))
-  (ANY "/hello" [] (resource :available-media-types ["text/html"]
-                             :handle-ok "<html><h1>Hello, Liberator!</h1></html>")))
+  (ANY "/hello" [] (resource
+                    :allowed-methods [:get :head]
+                    :available-media-types ["text/html"]
+                    :handle-ok "<html><h1>Hello, Liberator!</h1></html>"))
+  (ANY "/hello/:name" [name] (parameter name))
+  )
 (def handler
   (-> app
       wrap-params))
