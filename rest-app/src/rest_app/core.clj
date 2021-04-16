782ed9b496aab027c9fe38aa91428d889472626b
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-16 15:38:33 +0530

    updated

--- rest-app/src/rest_app/core.clj
+++ rest-app/src/rest_app/core.clj
@@ -1,54 +1,39 @@
 (ns rest-app.core (:require [liberator.core :refer [resource defresource]]
-                                 [liberator.dev :refer [wrap-trace]]
-                                 [ring.middleware.params :refer [wrap-params]]
-                                 [compojure.core :refer [defroutes ANY]]
-                                 [clojure-csv.core :as csv]))
+                            [liberator.dev :refer [wrap-trace]]
+                            [ring.middleware.params :refer [wrap-params]]
+                            [compojure.core :refer [defroutes ANY]]
+                            [clojure-csv.core :as csv]))
 
-;; (def customer-ref (ref {:siva {:name "Siva" :city "Bangalore"} 
-;;                         :vijay {:name "Vijay" :city "Bangalore"}}))
 
-;; (defresource customers
-;;   :allowed-methods [:get]
-;;   :available-media-types ["application/json"]
-;;   :exists? (fn [_] (let [result @customer-ref]
-;;                      (if-not (nil? result) {::entry result})))
-;;   :handle-ok ::entry
-;;   :handle-not-found {:error "ID is not found"}
-;; )
-
-(def employees (atom []))
+(def employees-ref (atom []))
 
 (defn addemployee [empid name]
-  (swap! employees conj {:empid empid :empname name}))
+  (swap! employees-ref conj {:empid empid :empname name}))
 
-(addemployee "Q123" "Rakesh Sharma")
+(addemployee "1" "Rakesh Sharma")
 
-(defn getemployees [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body  @employees})
+(defresource employees []
+  :allowed-methods [:get :post]
+  :available-media-types ["application/json"]
+  :post! @employees-ref
+  :post-redirect? (fn [ctx] {:location (format "/employees/1")})
+  :handle-ok @employees-ref
+  :handle-not-found {:error "ID is not found"})
 
-
-(defn addemployeetoatom [empid empname]
-  (swap! employees conj {:empid empid :empname empname}))
+(defresource employee [empid]
+  :allowed-methods [:get :put :delete]
+  :available-media-types ["application/json"]
+  :exists? (fn [_] (let [result (get @employees-ref empid)]
+                      (if-not (nil? result) {::entry result}) 
+                      ))
+  :handle-ok ::entry
+  :handle-not-found {:error "ID is not found"})
 
 (defroutes app
-   (ANY "/employees" []
-     (resource
-      :allowed-methods [:post :get]
-      :available-media-types ["application/json"]
-      :handle-ok @employees
-      
-      ;; :post! (fn [ctx]
-      ;;          (dosync
-      ;;           (let [body (slurp (get-in ctx [:request :body]))
-      ;;                 id   (count (alter posts conj body))]
-      ;;             {::id id})))
-        ;; actually http requires absolute urls for redirect but let's
-        ;; keep things simple.
-      ;;:post-redirect? (fn [ctx] {:location (format "/postbox/%s" (::id ctx))})
-      ))
-  )
+  (ANY "/" [] (resource))
+  (ANY "/employees" [] (employees))
+  (ANY "/employees/:empid" [empid] (employee empid)))
+
 
 (def handler
   (-> app
