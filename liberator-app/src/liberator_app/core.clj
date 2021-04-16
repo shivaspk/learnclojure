60a352072b234be10f9ef84b6d1d3ebd3edfb859
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-16 11:56:39 +0530

    a resource added

--- liberator-app/src/liberator_app/core.clj
+++ liberator-app/src/liberator_app/core.clj
@@ -2,8 +2,9 @@
                                  [ring.middleware.params :refer [wrap-params]]
                                  [compojure.core :refer [defroutes ANY]]))
 (defroutes app
-  (ANY "/" [] (resource)))
-
+  (ANY "/" [] (resource))
+  (ANY "/hello" [] (resource :available-media-types ["text/html"]
+                             :handle-ok "<html><h1>Hello, Liberator!</h1></html>")))
 (def handler
   (-> app
       wrap-params))
