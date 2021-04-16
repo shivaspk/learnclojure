6b74ed0bd87612649250a3943dcee14da6a08a5d
Siva Prasanna <shivaspk.siva@gmail.com>
2021-04-16 12:20:25 +0530

    content nego

--- liberator-app/src/liberator_app/core.clj
+++ liberator-app/src/liberator_app/core.clj
@@ -13,6 +13,18 @@
                     :available-media-types ["text/html"]
                     :handle-ok "<html><h1>Hello, Liberator!</h1></html>"))
   (ANY "/hello/:name" [name] (parameter name))
+  (ANY "/babel" []
+    (resource :available-media-types ["text/plain" "text/html"
+                                      "application/json" "application/clojure;q=0.9"]
+              :handle-ok
+              #(let [media-type
+                     (get-in % [:representation :media-type])]
+                 (condp = media-type
+                   "text/plain" "You requested plain text"
+                   "text/html" "<html><h1>You requested HTML</h1></html>"
+                   {:message "You requested a media type"
+                    :media-type media-type}))
+              :handle-not-acceptable "Uh, Oh, I cannot speak those languages!"))
   )
 (def handler
   (-> app
